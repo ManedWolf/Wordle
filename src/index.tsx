cca2723939e14367758957ea4e40c1cda2b94f8f
ManedWolf <142830732+ManedWolf@users.noreply.github.com>
2023-09-15 00:32:15 +0200

    Providers added

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {CssVarsProvider} from "@mui/joy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider>
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   </React.StrictMode>
 );
 
