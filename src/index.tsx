0dc88fbffd81a890c555c9428f5fb9a32cdf6eab
ManedWolf <142830732+ManedWolf@users.noreply.github.com>
2023-09-15 01:02:29 +0200

    Project restructuring

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/Home/App';
+import reportWebVitals from './utils/reportWebVitals';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {CssVarsProvider} from "@mui/joy";
 
