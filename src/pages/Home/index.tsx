ca1844a8a45831540350bf31f64a6b3c3b1abb8c
ManedWolf <142830732+ManedWolf@users.noreply.github.com>
2023-09-18 23:13:29 +0200

    Simple Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,61 +5,82 @@ import {Box, Button, Stack, Typography} from "@mui/joy";
 
 
 const Home = () =>
-  <Box
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    minWidth="100vh"
-    minHeight="100vh"
-  >
     <Stack
+      display="flex"
+      minWidth="100vh"
+      minHeight="100vh"
       direction="column"
-      alignItems="center"
-      justifyContent="center"
-      spacing={2}
     >
-      {/*LOGO*/}
-      <Typography level='h1'>
-        Wordle
-      </Typography>
-      <Typography level='h2'>
-        Get 6 chances to guess a 5-letter word.
-      </Typography>
+      <Box
+        width="100%"
+        sx={{'&:hover': {backgroundColor: 'blue'} }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          spacing={1}
+          margin='10px'
+          sx={{'&:hover': {backgroundColor: 'cyan'} }}
+        >
+          <Button
+            onClick={()=> null}
+            size="lg"
+            sx={{'&:hover': {backgroundColor: 'red'} }}
+          />
+          <Button
+            onClick={()=> null}
+            size="lg"
+            sx={{'&:hover': {backgroundColor: 'red'} }}
+          />
+        </Stack>
+      </Box>
       <Stack
-        direction="row"
+        direction="column"
         alignItems="center"
         justifyContent="center"
-        spacing={1}
+        spacing={2}
+        flexGrow="1"
       >
-        <Button
-          onClick={()=>null}
-          variant="solid"
-          color="primary"
-          size="lg"
+        {/*LOGO*/}
+        <Typography level='h1'>
+          Wordle
+        </Typography>
+        <Typography level='h2'>
+          Get X chances to guess a (X-1)-letter word.
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={1}
         >
-          Jouer
-        </Button>
-        <Button
-          onClick={()=>null}
-          variant="outlined"
-          color="neutral"
-          size="lg"
-        >
-          Comment Jouer
-        </Button>
-        <Button
-          onClick={()=>null}
-          variant="outlined"
-          color="neutral"
-          size="lg"
-        >
-          A propos
-        </Button>
+          <Button
+            onClick={()=>null}
+            variant="solid"
+            color="primary"
+            size="lg"
+          >
+            Jouer
+          </Button>
+          <Button
+            onClick={()=>null}
+            variant="outlined"
+            color="neutral"
+            size="lg"
+          >
+            Comment Jouer
+          </Button>
+          <Button
+            onClick={()=>null}
+            variant="outlined"
+            color="neutral"
+            size="lg"
+          >
+            A propos
+          </Button>
+        </Stack>
+        <Typography level='h3'>
+          {new Date().toLocaleDateString()}
+        </Typography>
       </Stack>
-      <Typography level='h3'>
-        {new Date().toLocaleDateString()}
-      </Typography>
     </Stack>
-  </Box>
 
 export default React.memo(Home);
